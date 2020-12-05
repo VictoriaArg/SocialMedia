3942a934f4b80d1d721f634e50d3a3790c3c9cbc
VictoriaArg <argasvic@live.com>
2020-12-05 19:39:00 -0300

    Setting up Mongoose and GQL

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const postSchema = new Schema({
+    body: String,
+    username: String,
+    createdAt: String,
+    comments: [
+        {
+            body: String,
+            username: String,
+            createdAt: String,
+        }
+    ],
+    likes: [
+        {
+            username: String,
+            createdAt: String
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
+});
+
+module.exports = mongoose.model('Post', postSchema);
