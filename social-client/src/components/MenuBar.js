1cac04e62b3cd534b78f78e3a06f07c56a312f69
VictoriaArg <argasvic@live.com>
2021-01-14 00:03:28 -0300

    fixed routes fro menubar

--- social-client/src/components/MenuBar.js
+++ social-client/src/components/MenuBar.js
@@ -3,13 +3,15 @@ import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState('');
+  const pathName = window.location.pathname;
+  const path = pathName === '/'? 'home' : pathName.substr(1); 
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
     return (
       <div>
-        <Menu pointing secondary>
+        <Menu pointing secondary size='massive' color='teal'>
           <Menu.Item
             name='home'
             active={activeItem === 'home'}
