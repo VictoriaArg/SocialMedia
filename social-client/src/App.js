f5bd6a41d0d267dbebc52316b9e464c848682160
VictoriaArg <argasvic@live.com>
2021-01-25 04:27:58 -0300

    login y register

--- social-client/src/App.js
+++ social-client/src/App.js
@@ -3,26 +3,30 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import { Container } from 'semantic-ui-react';
+import './App.css'
+
+import { AuthProvider } from './context/auth'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-
 import MenuBar from './components/MenuBar'
 
-import './App.css'
+
 
 
 function App() {
   return (
-    <Router>
+    <AuthProvider>
+      <Router>
       <Container>
       <MenuBar />
       <Route exact path='/' component={Home} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/register' component={Register} />
       </Container>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
